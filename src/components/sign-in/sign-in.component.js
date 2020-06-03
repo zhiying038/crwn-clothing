a1ec0565893e0c6640094a81f752e8d3e68d8ba4
Yoh Zhi Ying <zhiying038@gmail.com>
2020-06-03 10:44:17 +0800

    updating app to redirect to home

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -29,7 +29,7 @@ class SignIn extends React.Component {
   };
 
   handleChange = (event) => {
-    const { value, name } = event;
+    const { value, name } = event.target;
     this.setState({
       [name]: value,
     });
